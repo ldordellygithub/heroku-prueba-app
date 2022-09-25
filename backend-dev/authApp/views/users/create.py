5f31319b12da141331c023909ab9dee18ad06a0a
ldordelly <dordellyluis@gmail.com>
2022-09-25 16:56:26 -0500

    version estable-prueba 2.0

---
+++ backend-dev/authApp/views/users/create.py
@@ -0,0 +1,11 @@
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your tests here.
+
+
+def  create_user(request):
+    return HttpResponse("modulo de  ususarios ")
+
+
